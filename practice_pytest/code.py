518b9ed9320eacefbd1fdfa6a24ad78749e43096
fukudy <58599089+fukufuku66@users.noreply.github.com>
2022-01-09 16:10:12 +0900

    [rename]

---
+++ practice_pytest/code.py
@@ -0,0 +1,11 @@
+def code1(x, y):
+    """入力された2数の和を返す
+
+    Args:
+        x ([int]): [入力1]
+        y ([int]): [入力2]
+
+    Returns:
+        [int]: [入力1と入力2の和]
+    """
+    return x + y
---
+++ practice_pytest/test_code.py
@@ -0,0 +1,6 @@
+import pytest
+from code import code1
+
+def test_1():
+    assert 3 == code1(1, 2)
+
